3dd532dc1a098fb62a78c90715f7cf0742476375
Santhosh Babu <html5webster@gmail.com>
2014-07-27 15:26:03 +0530

    updated grunt lint file list

--- public/script/config/pathConfig.js
+++ public/script/config/pathConfig.js
@@ -1,63 +1,59 @@
-requirejs.config( {
-	  baseUrl: "/public"
-	, paths : {
-		// external librarires
-		"requireLib" 							: "script/lib/requirejs/require"
-		, "text" 								: "script/lib/requirejs/plugin/text.2.0.12"
-		, "jquery" 								: "script/lib/jquery/jquery.2.1.1"
-		, "google-code-prettify"				: "script/lib/googlePrettify/prettify"
-
-		// modules
-		, "init" 								: "script/module/init"
-		, "util" 								: "script/module/util"
-		, "loader" 								: "script/module/loader"
-
-		// templates
-		, "pageNavigation" 						: "tmpl/module/pageNavigation.tmpl"
-		, "pageHeader" 							: "tmpl/module/pageHeader.tmpl"
-		, "wikiHeader" 							: "tmpl/module/wikiHeader.tmpl"
-		, "wikiNavigation" 						: "tmpl/module/wikiNavigation.tmpl"
-		, "pageMarkup"		 					: "tmpl/module/pageMarkup.tmpl"
-		, "demoMarkup"		 					: "tmpl/module/demoMarkup.tmpl"
-		, "wikiMarkup"		 					: "tmpl/module/wikiMarkup.tmpl"
-
-		// snippets
-		, "htmlSample" 							: "uiSnippet/sample/markup.tmpl"
-		, "cssSample" 							: "uiSnippet/sample/style.css"
-		, "scriptSample"						: "uiSnippet/sample/behaviour"
-		, "scriptSampletmpl"					: "uiSnippet/sample/behaviour.js"
-		, "notesSample"							: "uiSnippet/sample/notes.tmpl"
- 	
- 		// Grid Using Float
-		, "htmlGridUsingFloatTwoColumn"			: "uiSnippet/gridUsingFloat/markupTwoColumn.tmpl"
-		, "htmlGridUsingFloatThreeColumn"		: "uiSnippet/gridUsingFloat/markupThreeColumn.tmpl"
-		, "htmlGridUsingFloatFourColumn" 		: "uiSnippet/gridUsingFloat/markupFourColumn.tmpl"
-		, "htmlGridUsingFloatFiveColumn" 		: "uiSnippet/gridUsingFloat/markupFiveColumn.tmpl"
-		, "cssGridUsingFloat" 					: "uiSnippet/gridUsingFloat/style.css"
-		, "notesGridUsingFloat"		 			: "uiSnippet/gridUsingFloat/notes.tmpl"
-		
-		// Truncate Text With Ellipse
-		, "cssTruncateTextWithEllipse" 			: "uiSnippet/truncateTextWithEllipse/style.css"
-		, "notesTruncateTextWithEllipse"		: "uiSnippet/truncateTextWithEllipse/notes.tmpl"
-
-		// Tabbed Navigation
-		, "htmlTabbedNavigation" 				: "uiSnippet/tabbedNavigation/markup.tmpl"
-		, "cssTabbedNavigation" 				: "uiSnippet/tabbedNavigation/style.css"
-		, "scriptTabbedNavigation" 				: "uiSnippet/tabbedNavigation/behaviour"
-		, "scriptTabbedNavigationtmpl" 			: "uiSnippet/tabbedNavigation/behaviour.js"
-		, "notesTabbedNavigation"				: "uiSnippet/tabbedNavigation/notes.tmpl"
-
-		// Slide Box
-		, "htmlSlideBox" 						: "uiSnippet/SlideBox/markup.tmpl"
-		, "cssSlideBox" 						: "uiSnippet/SlideBox/style.css"
-		, "scriptSlideBox" 						: "uiSnippet/SlideBox/behaviour"
-		, "scriptSlideBoxtmpl" 					: "uiSnippet/SlideBox/behaviour.js"
-		, "notesSlideBox"						: "uiSnippet/SlideBox/notes.tmpl"
-
-	  }
-	, shim: {
-		
-	  }
-	, urlArgs : "bust=" + ( new Date() ).getTime()
-
+require.config( {
+    baseUrl: "/public",
+    paths: {
+        // external librarires
+        "requireLib": "script/lib/requirejs/require",
+        "text": "script/lib/requirejs/plugin/text.2.0.12",
+        "jquery": "script/lib/jquery/jquery.2.1.1",
+        "google-code-prettify": "script/lib/googlePrettify/prettify"
+        // modules
+        ,
+        "init": "script/module/init",
+        "util": "script/module/util",
+        "loader": "script/module/loader"
+        // templates
+        ,
+        "pageNavigation": "tmpl/module/pageNavigation.tmpl",
+        "pageHeader": "tmpl/module/pageHeader.tmpl",
+        "wikiHeader": "tmpl/module/wikiHeader.tmpl",
+        "wikiNavigation": "tmpl/module/wikiNavigation.tmpl",
+        "pageMarkup": "tmpl/module/pageMarkup.tmpl",
+        "demoMarkup": "tmpl/module/demoMarkup.tmpl",
+        "wikiMarkup": "tmpl/module/wikiMarkup.tmpl"
+        // snippets
+        ,
+        "htmlSample": "uiSnippet/sample/markup.tmpl",
+        "cssSample": "uiSnippet/sample/style.css",
+        "scriptSample": "uiSnippet/sample/behaviour",
+        "scriptSampletmpl": "uiSnippet/sample/behaviour.js",
+        "notesSample": "uiSnippet/sample/notes.tmpl"
+        // Grid Using Float
+        ,
+        "htmlGridUsingFloatTwoColumn": "uiSnippet/gridUsingFloat/markupTwoColumn.tmpl",
+        "htmlGridUsingFloatThreeColumn": "uiSnippet/gridUsingFloat/markupThreeColumn.tmpl",
+        "htmlGridUsingFloatFourColumn": "uiSnippet/gridUsingFloat/markupFourColumn.tmpl",
+        "htmlGridUsingFloatFiveColumn": "uiSnippet/gridUsingFloat/markupFiveColumn.tmpl",
+        "cssGridUsingFloat": "uiSnippet/gridUsingFloat/style.css",
+        "notesGridUsingFloat": "uiSnippet/gridUsingFloat/notes.tmpl"
+        // Truncate Text With Ellipse
+        ,
+        "cssTruncateTextWithEllipse": "uiSnippet/truncateTextWithEllipse/style.css",
+        "notesTruncateTextWithEllipse": "uiSnippet/truncateTextWithEllipse/notes.tmpl"
+        // Tabbed Navigation
+        ,
+        "htmlTabbedNavigation": "uiSnippet/tabbedNavigation/markup.tmpl",
+        "cssTabbedNavigation": "uiSnippet/tabbedNavigation/style.css",
+        "scriptTabbedNavigation": "uiSnippet/tabbedNavigation/behaviour",
+        "scriptTabbedNavigationtmpl": "uiSnippet/tabbedNavigation/behaviour.js",
+        "notesTabbedNavigation": "uiSnippet/tabbedNavigation/notes.tmpl"
+        // Slide Box
+        ,
+        "htmlSlideBox": "uiSnippet/SlideBox/markup.tmpl",
+        "cssSlideBox": "uiSnippet/SlideBox/style.css",
+        "scriptSlideBox": "uiSnippet/SlideBox/behaviour",
+        "scriptSlideBoxtmpl": "uiSnippet/SlideBox/behaviour.js",
+        "notesSlideBox": "uiSnippet/SlideBox/notes.tmpl"
+    },
+    shim: {},
+    urlArgs: "bust=" + ( new Date() ).getTime()
 } );
